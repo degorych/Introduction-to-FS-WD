071045915b0fc9dab3c6bd9b7f0449675d964efb
degorych <degorych@gmail.com>
2018-05-23 22:56:49 +0300

    Update vote check

--- WD_PS4_PHP_JSON/vote/js/pieCharts.js
+++ WD_PS4_PHP_JSON/vote/js/pieCharts.js
@@ -1,22 +1,20 @@
-let voteData = [
-    ["Vote variant", "result"]
-];
+let voteData = [];
+const voteTitle = ["Vote variant", "result"];
+const piechart = document.getElementById("piechart");
 
 fetch("json/data.json")
     .then(response => response.json())
     .then(function (data) {
-        let valueArr = []; // For check
         for (let key in data) {
             voteData.push([key, data[key]]);
-            valueArr.push(data[key]);
         }
-        // If json not exist and nobody voted, pie chart is invisible
-        const piechart = document.getElementById("piechart");
-        if (valueArr.every(value => (value === 0))) {
+        // If nobody voted, pie chart is invisible
+        if (voteData.every(value => (value[1] === 0))) {
             piechart.style.display = "none";
         }
+        voteData.unshift(voteTitle);
     })
-    .then(function() {
+    .then(function () {
         google.charts.load("current", {packages:["corechart"]});
         google.charts.setOnLoadCallback(drawChart);
 
@@ -34,7 +32,7 @@ fetch("json/data.json")
                 titleTextStyle: {color:"#ff1763", fontName: "Verdana", fontSize: 20},
             };
 
-            const chart = new google.visualization.PieChart(document.getElementById("piechart"));
+            const chart = new google.visualization.PieChart(piechart);
             chart.draw(data, options);
         }
     });
