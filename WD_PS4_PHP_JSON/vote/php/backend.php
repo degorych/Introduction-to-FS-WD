151c0c4139d61d7c583d30b3fa6d5611d7d063c2
degorych <degorych@gmail.com>
2018-05-28 11:12:56 +0300

    Update if (empty($_POST)) in backend

--- WD_PS4_PHP_JSON/vote/php/backend.php
+++ WD_PS4_PHP_JSON/vote/php/backend.php
@@ -1,17 +1,22 @@
 <?php
 $rout = (empty($_POST)) ? "../index.html" : "../result.php";
+if (!empty($_POST)) {
+	$rout = "../result.php";
+	include_once "createJson.php";
+	$file = "../json/data.json";
+	createJson($file);
 
-include_once "createJson.php";
-$file = "../json/data.json";
-createJson($file);
+	$data = $_POST["vote-variants"];
 
-$data = $_POST["vote-variants"];
-
-$openFile = file_get_contents($file);
-$voteData = json_decode($openFile, true);
-if (array_key_exists($data, $voteData)) {
-	$voteData[$data] += 1;
-	file_put_contents($file, json_encode($voteData, JSON_PRETTY_PRINT));
+	$openFile = file_get_contents($file);
+	$voteData = json_decode($openFile, true);
+	if (array_key_exists($data, $voteData)) {
+		$voteData[$data] += 1;
+		file_put_contents($file, json_encode($voteData, JSON_PRETTY_PRINT));
+	}
+	unset($_POST["vote-variants"]);
+} else {
+	$rout = "../index.html";
 }
-unset($_POST["vote-variants"]);
+
 header("Location:".$rout);
