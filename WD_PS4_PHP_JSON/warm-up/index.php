37658a23e6088d954fdb3c060ed091ef3466c090
degorych <degorych@gmail.com>
2018-06-02 21:14:18 +0300

    Reformat output, add buttons

--- WD_PS4_PHP_JSON/warm-up/index.php
+++ WD_PS4_PHP_JSON/warm-up/index.php
@@ -1,91 +1,97 @@
 <?php
-include_once("./php/functions.php");
-
-define("PAINTCHESS", "0");
-define("DIGITSUM", "1");
-
-if (isset($_POST["task"])) {
-    $resultFunc = 0;
-    $taskValue = $_POST["task"];
-    switch ($taskValue) {
-        case PAINTCHESS: $resultFunc = paintChess();
-            break;
-        case DIGITSUM: $resultFunc = digitSum();
-            break;
-				default: $resultFunc = "function error";
-    }
-}
+session_start();
 ?>
 <!doctype html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport"
-					content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<title>WD_PS4 PHP, JSON</title>
-		<link rel="stylesheet" href="css/style.css">
-	</head>
-	<body>
-		<div class="1task">
-			<h2>1. Я считаю сумму чисел от "-1000" до "1000"</h2>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>WD_PS4 PHP, JSON</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="1task">
+    <h2>1. Я считаю сумму чисел от &#34;-1000&#34; до &#34;1000&#34;</h2>
+    <form action="php/functions.php" method="post">
+        <input type="hidden" name="task" value="sum"/>
+        <input type="submit" value="Посчитать"/>
+    </form>
+    <div>
         <?php
-        echo $sum;
+        if (isset($_SESSION['sum'])) echo $_SESSION['sum'];
         ?>
-		</div>
-		<div class="2task">
-			<h2>2. Я считаю сумму чисел, оканчивающихся на 2, 3, 7</h2>
+    </div>
+</div>
+<div class="2task">
+    <h2>2. Я считаю сумму чисел, оканчивающихся на 2, 3, 7</h2>
+    <form action="php/functions.php" method="post">
+        <input type="hidden" name="task" value="sum237"/>
+        <input type="submit" value="Посчитать"/>
+    </form>
+    <div>
         <?php
-        echo $sum237;
+        if (isset($_SESSION['sum237'])) echo $_SESSION['sum237'];
         ?>
-		</div>
-		<div class="3task">
-			<h2>3. Я рисую картинку из 50 строк со звездами</h2>
-				<?php
-        echo $star;
-				?>
-		</div>
-		<div class="4task">
-			<h2>4. Я рисую шахматные доски</h2>
-			<form method="post">
-				<label>Введите размер доски:
-					<input type="number" name="chess-number-row" value="4" pattern="\d+" required/>
-					<span>Х</span>
-					<input type="number" name="chess-number-column" value="4" pattern="\d+" required/>
-				</label>
-				<input type="hidden" name="task" value="<?php echo PAINTCHESS; ?>"/>
-				<input type="submit" name="but" value="Нарисовать"/>
-			</form>
-			<div class="chess-board">
-				<?php
-				if ($taskValue === PAINTCHESS) {
-					echo $resultFunc;
-				}
-				?>
-			</div>
-		</div>
-		<div class="5task">
-			<h2>5. Я считаю сумму цифр числа</h2>
-			<form method="post">
-				<label>Введите число
-					<input type="number" name="user-number" value="1234" pattern="\d+" required/>
-				</label>
-				<input type="hidden" name="task" value="<?php echo DIGITSUM; ?>">
-				<input type="submit" name="but" value="Считать"/>
-			</form>
+    </div>
+</div>
+<div class="3task">
+    <h2>3. Я рисую картинку из 50 строк со звездами</h2>
+    <form action="php/functions.php" method="post">
+        <input type="hidden" name="task" value="star"/>
+        <input type="submit" value="Нарисовать"/>
+    </form>
+    <div>
         <?php
-        if ($taskValue === DIGITSUM) {
-            echo $resultFunc;
-        }
+        if (isset($_SESSION['star'])) echo $_SESSION['star'];
         ?>
-		</div>
-		<div class="6task">
-			<h2>5. Я создаю и сортирую массив</h2>
-				<?php
-        foreach ($uniqRandomArr as $key => $val) {
-            echo "$val key: $key"."<br>"; // Key - index in array
-        }
-				?>
-		</div>
-	</body>
+    </div>
+</div>
+<div class="4task">
+    <h2>4. Я рисую шахматные доски</h2>
+    <form action="php/functions.php" method="post">
+        <label>Введите размер доски:
+            <input type="number" name="chess-number-row" value="4" pattern="\d+" required/>
+            <span>Х</span>
+            <input type="number" name="chess-number-column" value="4" pattern="\d+" required/>
+        </label>
+        <input type="hidden" name="task" value="paintChess"/>
+        <input type="submit" value="Нарисовать"/>
+    </form>
+    <div class="chess-board">
+        <?php
+        if (isset($_SESSION['paintChess'])) echo $_SESSION['paintChess'];
+        ?>
+    </div>
+</div>
+<div class="5task">
+    <h2>5. Я считаю сумму цифр числа</h2>
+    <form action="php/functions.php" method="post">
+        <label>Введите число
+            <input type="number" name="user-number" value="1234" pattern="\d+" required/>
+        </label>
+        <input type="hidden" name="task" value="digitSum">
+        <input type="submit" value="Считать"/>
+    </form>
+    <div>
+        <?php
+        if (isset($_SESSION['digitSum'])) echo $_SESSION['digitSum'];
+        ?>
+    </div>
+</div>
+<div class="6task">
+    <h2>6. Я создаю и сортирую массив</h2>
+    <form action="php/functions.php" method="post">
+        <input type="hidden" name="task" value="sortArray"/>
+        <input type="submit" value="Создать"/>
+    </form>
+    <div>
+        <?php
+        if (isset($_SESSION['sortArray'])) print_r($_SESSION['sortArray']);
+        session_destroy();
+        ?>
+    </div>
+</div>
+</body>
 </html>
--- WD_PS4_PHP_JSON/warm-up/php/functions.php
+++ WD_PS4_PHP_JSON/warm-up/php/functions.php
@@ -1,120 +1,116 @@
 <?php
-if (stristr($_SERVER["PHP_SELF"], "functions.php")) {
-    header("Location:../index.php");
+session_start();
+
+if (isset($_POST['task'])) {
+    $taskValue = $_POST['task'];
+    switch ($taskValue) {
+        case 'sum':
+            $resultFunc = sumElements();
+            break;
+        case 'sum237':
+            $resultFunc = sumElements([2, 3, 7]);
+            break;
+        case 'star':
+            $resultFunc = createStar();
+            break;
+        case 'paintChess':
+            $resultFunc = paintChess();
+            break;
+        case 'digitSum':
+            $resultFunc = digitSum();
+            break;
+        case 'sortArray':
+            $resultFunc = createRandomArr();
+            break;
+    }
+    $_SESSION[$taskValue] = $resultFunc;
 }
+header('Location:../index.php');
 
-/* --- Task 1 --- */
+/* --- Task 1, 2 --- */
 
-function sumElements() {
-    $minNum = -1000;
-    $maxNum = 1000;
+function sumElements($elements = [])
+{
     $sum = 0;
 
-    while ($minNum <= $maxNum) {
-        $sum += $minNum;
-        $minNum++;
+    for ($i = -1000; $i >= 1000; $i++) {
+        if (!empty($elements)) {
+            $lastDigit = abs($i) % 10;
+            foreach ($elements as $value) {
+                if ($lastDigit === $value) $sum += $i;
+            }
+        } else {
+            $sum += $i;
+        }
     }
     return $sum;
 }
 
-$sum = sumElements();
-
-/* --- Task 2 --- */
-
-function sumElements237() {
-    $minNum = -1000;
-    $maxNum = 1000;
-    $sum237 = 0;
-
-    while ($minNum <= $maxNum) {
-        $minNumLastDigit = abs($minNum) % 10;
-        if ($minNumLastDigit === 2 || $minNumLastDigit === 3 || $minNumLastDigit === 7) {
-            $sum237 += $minNum;
-        }
-        $minNum++;
-    }
-    return $sum237;
-}
-
-$sum237 = sumElements237();
-
 /* --- Task 3 --- */
 
-function createStar() {
-    define("ELEMENTS", 50);
-    $star = "";
-    for ($row = 1; $row <= ELEMENTS; $row++) {
-        for ($column = 1; $column <= $row; $column++) {
-            $star .= "*";
-        }
-        $star .= "<br>";
+function createStar()
+{
+    $star = '';
+    for ($row = 1; $row <= 50; $row++) {
+        $star .= str_repeat('*', $row).'<br>';
     }
     return $star;
 }
 
-$star = createStar();
-
 /* --- Task 4 --- */
 
-function paintChess() {
-    $rows = $_POST["chess-number-row"];
-    $columns = $_POST["chess-number-column"];
+function paintChess()
+{
+    $rows = $_POST['chess-number-row'];
+    $columns = $_POST['chess-number-column'];
 
-    if (!ctype_digit($rows) || !ctype_digit($columns)) {
-        return "Your data must be positive integer";
+    if (!is_numeric($rows) || !is_numeric($columns)) {
+        return 'Your data must be positive integer';
     }
 
-    define("MINVALUE", 1);
-    define("MAXVALUE", 100);
+    $minValue = 1;
+    $maxValue = 1000;
 
-    if ($rows > MAXVALUE || $columns > MAXVALUE || $rows < MINVALUE || $columns < MINVALUE) {
-        return "Your data may be from 1 to 100";
+    if ($rows > $maxValue || $columns > $maxValue || $rows < $minValue || $columns < $minValue) {
+        return "Your data may be from $minValue to $maxValue";
     }
 
-    $result = "";
-    while ($rows > 0) {
-        $result .= "<div>";
-        $rows--;
+    $result = '';
+    for ($i = $rows; $i > 0; $i--) {
+        $result .= '<div>';
 
         for ($j = 0; $j < $columns; $j++) {
-            if (($j + $rows) % 2 === 0) {
-                $result .= "<div style='background-color: black '></div>";
-            }
-            else {
-                $result .= "<div style='background-color: white '></div>";
-            }
+            $color = (($j + $i) % 2 === 0) ? 'black' : 'white';
+            $result .= "<div style='background-color: {$color}'></div>";
         }
-        $result .= "</div>";
+        $result .= '</div>';
     }
     return $result;
-}
+};
 
 /* --- Task 5 --- */
 
-function digitSum() {
-	$digitSum = (string)abs($_POST["user-number"]);
+function digitSum()
+{
+    $digits = abs($_POST['user-number']);
 
-    if (!ctype_digit($digitSum)) {
-        return "Your data must be integer";
+    if (!is_numeric($digits)) {
+        return 'Your data must be integer';
     }
 
-    $digitSumArr = str_split($digitSum);
-    return array_sum($digitSumArr);
-}
+    return array_sum(str_split($digits));
+};
 
 /* --- Task 6 --- */
 
-function createRandomArr() {
+function createRandomArr()
+{
     $randomArr = [];
 
     for ($i = 0; $i < 100; $i++) {
-        $randomArr[] = rand(1, 10);
+        $randomArr[] = mt_rand(1, 10);
     }
 
-    $uniqRandomArr = array_unique($randomArr);
-    arsort($uniqRandomArr);
-
-    return $uniqRandomArr;
+    arsort($randomArr);
+    return array_unique($randomArr);
 }
-
-$uniqRandomArr = createRandomArr();
