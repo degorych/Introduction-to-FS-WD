8d00ff6b4cec4e7a5396fe8981c0335abac9380e
degorych <degorych@gmail.com>
2018-06-21 22:54:27 +0300

    Add spaces

--- WD_PS4_PHP_JSON/vote/public/php/getJson.php
+++ WD_PS4_PHP_JSON/vote/public/php/getJson.php
@@ -2,6 +2,6 @@
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header('Location: ../index.php');
 }
-$file = __DIR__.'\..\..\private\json\data.json';
+$file = __DIR__ . '\..\..\private\json\data.json';
 header('Content-Type: application/json');
 echo file_get_contents($file);
