6a6edf28673d5da35845262e89f17c31071a2f55
degorych <degorych@gmail.com>
2018-06-21 22:55:33 +0300

    Add new paths

--- WD_PS4_PHP_JSON/vote/private/php/config.php
+++ WD_PS4_PHP_JSON/vote/private/php/config.php
@@ -1,9 +1,11 @@
 <?php
 define('PATH_TO_DIR', __DIR__);
 return array(
-    'createJson' => PATH_TO_DIR.'\createJson.php',
-    'voteCounter' => PATH_TO_DIR.'\voteCounter.php',
-    'jsonFile' => PATH_TO_DIR.'\..\json\data.json',
+    'createJson' => PATH_TO_DIR . '\createJson.php',
+    'createVote' => PATH_TO_DIR . '\createVote.php',
+    'voteCounter' => PATH_TO_DIR . '\voteCounter.php',
+    'jsonFile' => PATH_TO_DIR . '\..\json\data.json',
+    'variants' => PATH_TO_DIR . '\variants.php',
     'js' => 'js/pieCharts.js',
     'main' => 'index.php'
 );
