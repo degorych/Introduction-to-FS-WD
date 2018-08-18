c875f46d3f013d8f1d8366ec5feaf330501156da
degorych <degorych@gmail.com>
2018-08-18 16:14:10 +0300

    Make 20 points

--- WD_ST3_Draggable_Speech_Balloons/js/script.js
+++ WD_ST3_Draggable_Speech_Balloons/js/script.js
@@ -2,28 +2,31 @@ $(function () {
     const container = $("#container");
     
     function getCursorPosition(event) {
-        return {x: event.pageX, y: event.pageY};
+        const parentCoors = container.offset();
+        return {x: event.pageX - parentCoors.left, y: event.pageY - parentCoors.top};
     }
     
     function createBalloon(event) {
         return $("<div/>").addClass("ui-widget-content draggable")
             .css({
-                "top": getCursorPosition(event).y,
-                "left": getCursorPosition(event).x
+                "top": getCursorPosition(event).y - 10,
+                "left": getCursorPosition(event).x + 25
             })
             .append("<input>")
-            .draggable();
+            .draggable({containment: "parent", cursor: "move"});
     }
 
-    function saveMsg(obj, key) {
+    function saveMsg(obj, key = 13) {
         let value;
+
         if (key === 13) {
             value = obj.value;
         } else if (key === 27) {
-            value = getAttribute('value');
+            value = obj.getAttribute('value');
         } else {
             return;
         }
+
         obj.offsetParent.innerText = value;
         obj.remove();
     }
@@ -34,13 +37,12 @@ $(function () {
         } else if (event.target.className.indexOf("draggable") + 1) {
             const msg = event.target.innerText;
             event.target.innerText = "";
-            $("<input/>").attr("value", msg).val(msg).appendTo(event.target).trigger("focus");
-            console.log(event.target.children);
+            $("<input/>").attr("value", msg).val(msg).appendTo(event.target).trigger("select").trigger("focus");
         }
     });
 
     container.on("blur", "input", function () {
-        saveMsg(this, 13);
+        saveMsg(this);
     });
 
     container.on("keydown", "input", function (event) {
