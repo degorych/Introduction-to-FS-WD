576e92e24496bdaadefd138d5218a4a6cc4fbb77
degorych <degorych@gmail.com>
2018-09-04 00:28:35 +0300

    Add format text func, add hyphen script, fix bug then balloon save position out of container, add general ajax error func

--- WD_ST3_Draggable_Speech_Balloons/js/script.js
+++ WD_ST3_Draggable_Speech_Balloons/js/script.js
@@ -1,41 +1,109 @@
 $(function () {
     const container = $("#container");
     const body = $("body");
+    const badDbResponse = {
+        responseText: "Can not get speech balloons"
+    };
     let balloonId = 0;
 
-    function createBalloon(event) {
-        const parentCoors = container.offset();
-        return $("<div/>").attr("id", balloonId)
-            .addClass("ui-widget-content draggable")
-            .css({
-                "top": event.clientY - parentCoors.top,
-                "left": event.clientX - parentCoors.left,
-                "position": "absolute"
-            })
-            .append("<input>")
-            .draggable({containment: "parent"});
+    function formatText(message) {
+        const maxMessageLens = 100;
+        const maxWordLens = 15;
+        const hyphenLens = 7;
+
+        message = message.split(" ").reduce(function (msg, word) {
+            if (word.length >= maxWordLens) {
+                return msg + " " + word.match(new RegExp('(.{1,'+ maxWordLens +'})', 'g'))
+                    .reduce(function (accum, value) {
+
+                        const beforeHyphen = value.slice(0, hyphenLens);
+                        const afterHyphen = value.slice(hyphenLens, value.length-1);
+
+                        return accum + beforeHyphen + "\u00AD" + afterHyphen;
+                    }, "\u00AD");
+            } else {
+                return msg + " " + word;
+            }
+        }, "");
+
+        if (message.length > maxMessageLens) {
+            message = message.slice(0, maxMessageLens) + "...";
+        }
+
+        return message;
     }
 
-    function createBalloonFromServer(params) {
+    const failResponseHandler = function (response) {
+        const errorSelector = response.status === 404 ? badDbResponse : response;
+        const createErrorEl = $("<div/>").addClass("error")
+            .text(errorSelector.responseText)
+            .appendTo(body);
+        setTimeout(function () {
+            createErrorEl.hide(400);
+        }, 3000);
+    };
+
+    function getBalloonsFromDB() {
+        $.ajax({
+            url: "data/data.json",
+            method: "GET",
+            dataType: "json"
+        }).done(function (response) {
+            for (let i in response) {
+                createSpeechBalloon(response[i]).appendTo(container)
+                    .hyphenate('en-us');
+            }
+        }).fail(failResponseHandler);
+    }
+
+    function createSpeechBalloon(params) {
         balloonId = parseInt(params.id) + 1;
-        return $("<div/>").attr("id", params.id)
+        const newBalloon = $("<div/>").attr("id", params.id)
             .addClass("ui-widget-content draggable")
             .css({
-                "top": parseInt(params.position.positionY),
-                "left": parseInt(params.position.positionX),
-                "position": "absolute"
+                top: parseInt(params.position.positionY),
+                left: parseInt(params.position.positionX),
+                position: "absolute"
             })
-            .text(params.message)
             .draggable({containment: "parent"});
+
+        !params.message ? newBalloon.append("<input>") : newBalloon.text(formatText(params.message));
+
+        return newBalloon;
     }
 
-    function saveMsg(obj, key = 13) {
+    function checkOutBorder(obj) {
+        const balloonCoors = $(obj).position();
+        const containerCoors = container.offset();
+
+        const rightPos = container.outerWidth() - (balloonCoors.left + $(obj).outerWidth(true));
+        const bottomPos = container.outerHeight() - (balloonCoors.top + $(obj).outerHeight(true));
+
+        const absoluteCoors = {
+            top: containerCoors.top + balloonCoors.top,
+            left: containerCoors.left + balloonCoors.left
+        };
+
+        if (rightPos < 0) {
+            absoluteCoors.left += rightPos;
+            $(obj).offset(absoluteCoors);
+        }
+        if (bottomPos < 0) {
+            absoluteCoors.top += bottomPos;
+            $(obj).offset(absoluteCoors);
+        }
+    }
+
+    const saveKey = 13;
+    const revertKey = 27;
+
+    function saveMsg(obj, key = saveKey) {
         let value = "";
         const input = obj.children[0];
 
-        if (key === 13) {
+        if (key === saveKey) {
             value = input.value;
-        } else if (key === 27) {
+        } else if (key === revertKey) {
             value = input.getAttribute("value");
         } else {
             return false;
@@ -43,8 +111,8 @@ $(function () {
 
         if (!value) {
             const info = {
-                "id": obj.getAttribute("id"),
-                "delete": true
+                id: obj.getAttribute("id"),
+                delete: 1
             };
             sendBalloonInfo(info);
 
@@ -52,20 +120,23 @@ $(function () {
             return false;
         }
 
+        $(obj).text(formatText(value)).hyphenate('en-us');
+        input.remove();
+
+        checkOutBorder(obj);
+
         const styles = getComputedStyle(obj);
         const info = {
-            "id": obj.getAttribute("id"),
-            "position": {
-                "positionX": styles.left,
-                "positionY": styles.top
+            id: obj.getAttribute("id"),
+            position: {
+                positionX: styles.left,
+                positionY: styles.top
             },
-            "message": value,
-            "delete": false
+            message: value,
+            delete: 0
         };
         sendBalloonInfo(info);
 
-        $(obj).text(value);
-        input.remove();
         return true;
     }
 
@@ -73,34 +144,37 @@ $(function () {
         return $.ajax({
             url: "server/server.php",
             method: "POST",
-            data: {"newInfo": info},
-            dataType: "json"
-        }).fail(function (response) {
-            $("<div/>").addClass("error").text(response.responseJSON).appendTo(body);
-        })
+            data: {newInfo: info},
+        }).fail(failResponseHandler)
     }
 
-    $.ajax({
-        url: "data/data.json",
-        method: "GET",
-        dataType: "json"
-    }).done(function (response) {
-        for (let i in response) {
-            createBalloonFromServer(response[i]).appendTo(container);
-        }
-    }).fail(function (response) {
-        $("<div/>").addClass("error").text(response).appendTo(body);
-    });
+    getBalloonsFromDB();
 
     container.on("dblclick", function (event) {
         const dblclickTarget = event.target;
+
         if (dblclickTarget === this) {
-            createBalloon(event).appendTo(container).children("input").trigger("focus");
-            balloonId++;
+            const parentCoors = container.offset();
+            const info = {
+                id: balloonId,
+                position: {
+                    positionX: event.clientX - parentCoors.left - 20,
+                    positionY: event.clientY - parentCoors.top - 50
+                },
+                message: "",
+                delete: 0
+            };
+            createSpeechBalloon(info).appendTo(container)
+                .children("input")
+                .trigger("focus");
         } else if (dblclickTarget.className.indexOf("draggable") + 1) {
             const msg = dblclickTarget.innerText;
             dblclickTarget.innerText = "";
-            $("<input/>").attr("value", msg).val(msg).appendTo(dblclickTarget).trigger("select").trigger("focus");
+            $("<input/>").attr("value", msg)
+                .val(msg)
+                .appendTo(dblclickTarget)
+                .trigger("select")
+                .trigger("focus");
         }
     });
 
@@ -128,13 +202,13 @@ $(function () {
 
         if (firstPos.top !== lastPos.top || firstPos.left !== lastPos.left) {
             const info = {
-                "id": currentBalloon.attr("id"),
-                "position": {
-                    "positionX": currentBalloon.css("left"),
-                    "positionY": currentBalloon.css("top")
+                id: currentBalloon.attr("id"),
+                position: {
+                    positionX: currentBalloon.css("left"),
+                    positionY: currentBalloon.css("top")
                 },
-                "message": currentBalloon.text(),
-                "delete": false
+                message: currentBalloon.text(),
+                delete: 0
             };
             sendBalloonInfo(info);
         }
--- WD_ST3_Draggable_Speech_Balloons/server/server.php
+++ WD_ST3_Draggable_Speech_Balloons/server/server.php
@@ -1,63 +1,67 @@
 <?php
+define('APP_PATH', __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
+define('GOOD_RESPONSE', 'save data');
+
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    header('Location: ' . __DIR__ . DIRECTORY_SEPARATOR .'..' . DIRECTORY_SEPARATOR. 'index.html');
+    header('Location: ' . APP_PATH . 'index.html');
     die();
-}
-
-$dataPath = __DIR__ . DIRECTORY_SEPARATOR .'..' . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'data.json';
-
-if (!file_exists($dataPath)) {
-    if (file_put_contents($dataPath, []) === false) {
-        $fileError = 'Data file can not be created';
-    }
-}
-
-if (!is_readable($dataPath)) {
-    $fileError = 'Data file is not readable';
-}
-
-if (!is_writable($dataPath)) {
-    $fileError = 'Data file is not writable';
-}
-
-if (isset($fileError)) {
-    http_response_code(400);
-    header('Content-Type: application/json');
-    echo json_encode($fileError);
-    return;
 }
 
 if (!isset($_POST['newInfo'])) {
     http_response_code(400);
-    header('Content-Type: application/json');
-    echo json_encode('no get data');
-    return;
+    echo 'No get data';
+    die();
 } else {
     $balloonInfo = $_POST['newInfo'];
 }
 
-$fullBalloonsData = json_decode(file_get_contents($dataPath),true);
+$dataPath = APP_PATH . 'data' . DIRECTORY_SEPARATOR . 'data.json';
+
+if (!file_exists($dataPath)) {
+    if (!file_put_contents($dataPath, json_encode([$balloonInfo], JSON_PRETTY_PRINT))) {
+        http_response_code(400);
+        echo 'Data file can not be created';
+        die();
+    } else {
+        echo GOOD_RESPONSE;
+        return;
+    }
+}
+
+if (!is_readable($dataPath) || !is_writable($dataPath)) {
+    http_response_code(400);
+    echo 'Can not work with data file';
+    return;
+}
+
+$fullBalloonsData = json_decode(file_get_contents($dataPath), true);
 
 if (!$fullBalloonsData) {
-    file_put_contents($dataPath, json_encode($balloonInfo, JSON_PRETTY_PRINT));
+    file_put_contents($dataPath, json_encode([$balloonInfo], JSON_PRETTY_PRINT));
+    echo GOOD_RESPONSE;
+    return;
 }
 
 $findBalloon = false;
+$deleteBalloon = (boolean) $balloonInfo['delete'];
 
+// Rewrite or delete balloon if it exists
 foreach ($fullBalloonsData as $index => $balloon) {
-    if ($balloon['id'] === $balloonInfo['id'] && $balloonInfo['delete'] === 'false') {
+    if ($balloon['id'] === $balloonInfo['id'] && !$deleteBalloon) {
         $fullBalloonsData[$index] = $balloonInfo;
         $findBalloon = true;
         break;
-    } elseif ($balloon['id'] === $balloonInfo['id'] && $balloonInfo['delete'] === 'true') {
+    } elseif ($balloon['id'] === $balloonInfo['id'] && $deleteBalloon) {
         unset ($fullBalloonsData[$index]);
         $findBalloon = true;
         break;
     }
 }
 
-if (!$findBalloon && $balloonInfo['delete'] === 'false') {
+// Add new balloon to db
+if (!$findBalloon && !$deleteBalloon) {
     $fullBalloonsData[] = $balloonInfo;
 }
+
 file_put_contents($dataPath, json_encode(array_values($fullBalloonsData), JSON_PRETTY_PRINT), LOCK_EX);
-echo json_encode('OK');
+echo GOOD_RESPONSE;
