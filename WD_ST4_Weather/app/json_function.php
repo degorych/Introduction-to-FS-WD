7df80eecd30fa99ea0a4c2798c2a919de007a290
degorych <degorych@gmail.com>
2018-09-19 00:42:03 +0300

    Add error handler into json function

--- WD_ST4_Weather/app/json_function.php
+++ WD_ST4_Weather/app/json_function.php
@@ -1,5 +1,9 @@
 <?php
 return function ($config) {
+    set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+        throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+    });
+
     if (!file_exists($config['jsonPath']) || !($jsonData = @file_get_contents($config['jsonPath'])) || !is_readable($config['jsonPath']) || !is_writable($config['jsonPath'])) {
         return 'Can not work with json file';
     }
@@ -10,19 +14,24 @@ return function ($config) {
         return 'Json file is empty';
     }
 
-    foreach ($jsonData['list'] as $value) {
-        $icon = array_search($value['weather'][0]['main'], $config['icons']);
+    try {
+        foreach ($jsonData['list'] as $value) {
+            $icon = array_search($value['weather'][0]['main'], $config['icons']);
 
-        if ($icon === 'Clouds' && $value['clouds']['all'] > 20) {
-            $icon .= '-sun';
+            if ($icon === 'Clouds' && $value['clouds']['all'] > 20) {
+                $icon .= '-sun';
+            }
+
+            $response[] = [
+                'time' => $value['dt'],
+                'temperature' => round($value['main']['temp'] - 273.15) + 0,
+                'icon' => $icon
+
+            ];
         }
-
-        $response[] = [
-            'time' => $value['dt'],
-            'temperature' => round($value['main']['temp'] - 273.15) + 0,
-            'icon' => $icon
-
-        ];
+    } catch (ErrorException $e) {
+        return 'Json data is incorrect';
     }
+
     return $response;
 };
